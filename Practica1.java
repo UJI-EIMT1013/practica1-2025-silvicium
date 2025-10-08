29b90b87078023658fe86834f1e1a3fc05f55ab2
Alex Popa <alexpopa7473@gmail.com>
2025-10-08 13:17:02 +0200

    Acabado EJ2

--- Practica1.java
+++ Practica1.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.Set;
+import java.util.HashSet;
 
 public class Practica1 {
 
@@ -21,9 +22,7 @@ public class Practica1 {
                 
                 if ((numActual % numVist) == 0) { //Si numActual/numVist el resto es 0, es multiplo
                     res.add(numActual);
-                }
-
-                if ( (numVist % numActual) == 0) { //Si numVist/numActual el resto es 0, es multiplo
+                }else if ( (numVist % numActual) == 0) { //Si numVist/numActual el resto es 0, es multiplo
                     res.add(numVist);
                 }
             }
@@ -35,6 +34,47 @@ public class Practica1 {
     //EJERCICIO2
     public static void separate (Set<Integer> cuadrados, Set<Integer> noCuadrados)  {
         
+        Set<Integer> union = new HashSet<Integer>();
+        union.addAll(cuadrados); //Añadimos cuadrados a union
+        union.addAll(noCuadrados); //Añadimos noCuadrados a union
+
+        Set<Integer> nuevoCuadrado = new HashSet<Integer>();
+        Set<Integer> nuevoNoCuadrado = new HashSet<Integer>();
+
+        boolean anadido = false;
+        
+        for (Integer x : union) { 
+            
+            for (Integer y : union) {
+                long yy = y*y; 
+                x.longValue(); //Convertimos x a long para tener suficiente espacio
+             
+                if ( yy == x.longValue() &&  !y.equals(x)) { //Gastamos .equals() para la comparacion de objetos
+                    nuevoCuadrado.add(x); 
+                    anadido = true;
+                    break;
+
+                }else if ( yy == x && y.equals(x)) {
+                    if (cuadrados.contains(x) && noCuadrados.contains(x)) {
+                        nuevoCuadrado.add(x);
+                        anadido = true;
+                        break;
+                    }
+                }
+            }
+
+            if(!anadido) {
+                nuevoNoCuadrado.add(x);
+            }
+        }
+
+        //Vaciamos los antiguos
+        cuadrados.clear();
+        noCuadrados.clear();
+
+        //Añadimos los nuevos numeros
+        cuadrados.addAll(nuevoCuadrado);
+        noCuadrados.addAll(nuevoNoCuadrado);
     }
 
     //EJERCICIO 3
